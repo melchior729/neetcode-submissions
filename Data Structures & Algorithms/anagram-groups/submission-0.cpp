4ad2e8bdc1b99344d133e38aa7e7902d077a67bb
Abhay Manoj <melchior88729@gmail.com>
2026-04-15 15:28:03 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+
+class Solution {
+public:
+/*
+
+Declare a map from string to vector<string>.
+iterate through the strings provided.
+sort the string alphabetically, this is the key
+append the non-sorted string to map[sorted_str].
+
+at the end, get the values, wrap in a vector, and return
+*/
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> sort_to_list;
+
+        for (const string& str : strs) {
+            string sorted = str;
+            std::sort(sorted.begin(), sorted.end());
+            sort_to_list[sorted].push_back(str);
+        }
+
+        vector<vector<string>> anagrams;
+
+        for (const auto& [key, val] : sort_to_list) {
+            anagrams.push_back(val);
+        }
+
+        return anagrams;
+    }
+};
