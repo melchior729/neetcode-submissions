bea2d34e3cbd655193b56f3d44b907910de315c8
Abhay Manoj <melchior88729@gmail.com>
2026-04-22 18:36:45 -0400

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.cpp
@@ -0,0 +1,62 @@
+class Solution {
+
+char DELIMITER = '#';
+
+public:
+    string encode(vector<string>& strs) {
+        string encoded{};
+        for (const auto& str : strs) {
+            encoded += to_string(str.length());
+            encoded += DELIMITER;
+            encoded += str;
+        }
+
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> decoded{};
+        string str{};
+
+        int n = s.length();
+        int i = 0;
+        int k = 0;
+        bool delimiter_read{};
+
+        while (i < n) {
+            
+            if (delimiter_read) {
+                str += s[i];
+
+                k--;
+                if (k == 0) {
+                    delimiter_read = false;
+                    decoded.push_back(str);
+                    str.clear(); 
+                }
+
+                i++;
+            } 
+            
+            else {
+                int j = i;
+                while (s[j] != DELIMITER) {
+                    j++;
+                }
+
+                string length_str = s.substr(i, j - i);
+                int length = stoi(length_str);
+
+                i = ++j; 
+                k = length;
+                delimiter_read = length != 0;
+
+                if (length == 0) {
+                    decoded.push_back("");
+                }
+            }
+        }
+
+        return decoded;
+    }
+};
