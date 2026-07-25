cf4de603bd79c82a4410a3a08ef3f91693eeee2e
Abhay Manoj <melchior88729@gmail.com>
2026-07-25 04:42:55 -0400

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::unordered_set<int> s{};
+        for (const auto num : nums) {
+            if (s.contains(num)) {
+                return true;
+            }
+
+            s.insert(num);
+        }
+        
+        return false;
+    }
+};
