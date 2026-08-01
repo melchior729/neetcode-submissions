4a42aef3c1ef317c7f0606dd62b060ca7df40ba7
Abhay Manoj <melchior88729@gmail.com>
2026-08-01 01:45:14 -0400

    Add: valid-sudoku - submission-6

---
+++ Data Structures & Algorithms/valid-sudoku/submission-6.cpp
@@ -0,0 +1,38 @@
+static constexpr auto BOARD_DIM{9};
+
+using Container = std::array<uint16_t, BOARD_DIM>;
+
+class Solution {
+   public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        Container row{};
+        Container col{};
+        Container box{};
+
+        for (auto i{0uz}; i < BOARD_DIM; ++i) {
+            for (auto j{0uz}; j < BOARD_DIM; ++j) {
+                auto entry{board[i][j]};
+                if (entry == '.') {
+                    continue;
+                }
+
+                auto shifted{(1 << ((entry - '0') - 1))};
+                auto in_row {row[i] & shifted};
+                auto in_col {col[j] & shifted};
+
+                auto b{3 * (i / 3) + (j / 3)};
+                auto in_box {box[b] & shifted};
+
+                if (in_row || in_col || in_box) {
+                    return false;
+                }
+
+                row[i] |= shifted;
+                col[j] |= shifted;
+                box[b] |= shifted;
+            }
+        }
+
+        return true;
+    }
+};
