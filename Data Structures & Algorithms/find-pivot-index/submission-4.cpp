cf81171911abf32d29230130ace47d338f4c2378
Abhay Manoj <melchior88729@gmail.com>
2026-08-04 01:01:33 -0400

    Add: find-pivot-index - submission-4

---
+++ Data Structures & Algorithms/find-pivot-index/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        auto size{nums.size()};
+        vector<int> left(size), right(size);
+
+        for (auto i{1uz}; i < size; ++i) {
+            left[i] = left[i - 1] + nums[i - 1];
+        }
+
+        for (auto i{size - 1}; i --> 0;) {
+            right[i] = right[i + 1] + nums[i + 1];
+        }
+
+        for (auto i{0uz}; i < size; ++i) {
+            if (left[i] == right[i]) {
+                return i;
+            }            
+        }
+
+        return -1;
+    }
+};
