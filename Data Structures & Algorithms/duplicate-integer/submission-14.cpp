e9489ca43c4fce8f742b43b1c125235268e90154
Abhay Manoj <melchior88729@gmail.com>
2026-08-10 23:03:14 -0400

    Add: duplicate-integer - submission-14

---
+++ Data Structures & Algorithms/duplicate-integer/submission-14.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> seen{};
+        for (const auto &n : nums) {
+            if (!seen.insert(n).second) {
+                return true;
+            }
+        } 
+
+        return false;
+    }
+};
