b6976460202021dc93e7842bbf5f0adfa50e5b03
Abhay Manoj <melchior88729@gmail.com>
2026-08-15 17:24:39 -0400

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res{};
+
+        sort(nums.begin(), nums.end());
+
+        for (auto i{0uz}; i < nums.size(); ++i) {
+            auto left{i + 1}, right{nums.size() - 1};
+            while (left < right) {
+                auto sum{nums[i] + nums[left] + nums[right]};
+                if (sum > 0) {
+                    right--; 
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    vector<int> sol{nums[i], nums[left], nums[right]};
+                    if (!ranges::contains(res, sol)) {
+                        res.push_back(sol);
+                    }
+
+                    left++;
+                    right--;
+                }
+            }
+        }
+
+        return res;
+    }
+};
