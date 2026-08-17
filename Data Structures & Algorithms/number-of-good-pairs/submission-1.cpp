97853d138a0a38dea7731641df7b84d947c29a62
Abhay Manoj <melchior88729@gmail.com>
2026-08-17 02:18:12 -0400

    Add: number-of-good-pairs - submission-1

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+   public:
+    int numIdenticalPairs(vector<int>& nums) {
+        auto pairs{0};
+        for (auto i{0uz}; i < nums.size(); ++i) {
+            for (auto j{i + 1}; j < nums.size(); ++j) {
+                pairs += (nums[i] == nums[j]);
+            }
+        }
+
+        return pairs;
+    }
+};
