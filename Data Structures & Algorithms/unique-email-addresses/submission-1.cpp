7b47024a1c404bfc6ce430164dc68812ddfef35f
Abhay Manoj <melchior88729@gmail.com>
2026-08-16 20:08:33 -0400

    Add: unique-email-addresses - submission-1

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-1.cpp
@@ -0,0 +1,64 @@
+class Solution {
+   public:
+    int numUniqueEmails(vector<string>& emails) {
+        /*
+        set of unique emails
+
+        for email in emails
+            local = ""
+            domain = ""
+
+            i = 0
+            while email[i] != '@':
+                if (email[i] == '+'):
+                    break
+                if (email[i] == '.'):
+                    continue
+                local += email[i]
+
+            while (i < email.size()):
+                domain += email[i]
+
+            seen.insert(local + domain)
+
+        return set.size()
+        */
+
+        unordered_set<string> stored{};
+
+        for (const auto& email : emails) {
+            auto local{""s};
+
+            auto i{0uz};
+            auto c{email[i]};
+            
+            while (c != '@') {
+                if (c == '+') {
+                    break;
+                } 
+ 
+                else if (c == '.') {
+                    i++;
+                    c = email[i];
+                    continue;
+                }
+
+                local += c;
+                i++;
+                c = email[i];
+            }
+
+            i = email.find('@') + 1;
+            stored.insert(local + (email.substr(i, email.length() - i)));
+        }
+
+        for (const auto & s : stored) {
+            cout << s << " ";
+        }
+
+        return stored.size();
+
+        // testemail@neetcode.com
+        // testemail@nee.tcode.com
+    }
+};
