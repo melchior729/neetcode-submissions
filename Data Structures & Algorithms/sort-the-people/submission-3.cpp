a4786cad0f7d4c00ecb6218ac171c206f4c1a601
Abhay Manoj <melchior88729@gmail.com>
2026-08-18 19:56:46 -0400

    Add: sort-the-people - submission-3

---
+++ Data Structures & Algorithms/sort-the-people/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<pair<int, int>> hcopy(heights.size());
+        for (auto i{0uz}; i < heights.size(); ++i) {
+            hcopy[i] = {heights[i], i};
+        } 
+
+        sort(hcopy.rbegin(), hcopy.rend());
+        vector<string> res(heights.size());
+
+        for (auto i{0uz}; i < heights.size(); ++i) {
+            res[i] = names[hcopy[i].second];
+       }
+
+        return res;
+    }
+};
