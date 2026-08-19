8cf3fc9e7ea9c63c20c6b981aeed27f8dbe08457
Abhay Manoj <melchior88729@gmail.com>
2026-08-19 13:51:17 -0400

    Add: take-gifts-from-the-richest-pile - submission-0

---
+++ Data Structures & Algorithms/take-gifts-from-the-richest-pile/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) { 
+        for (auto i{0}; i < k; ++i) {
+            sort(gifts.rbegin(), gifts.rend());
+            gifts[0] = static_cast<int>(sqrt(gifts[0]));
+        }      
+
+        auto sum{0};
+        for (const auto &gift : gifts) {
+            sum += gift;
+        }
+
+        return sum;
+    }
+};
