f681a9b9c1527481fcaed6ce397eb9c3fc168609
Abhay Manoj <melchior88729@gmail.com>
2026-08-19 19:35:24 -0400

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        auto n{static_cast<int>(digits.size())};
+        auto i{n - 1};
+
+        for (; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        if (i == -1) {
+            digits.insert(digits.begin(), 1);
+        }
+
+        return digits;
+    }
+};
