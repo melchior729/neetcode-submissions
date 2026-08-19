9f4659d186fa10131e0d249d256c1bc1cff5401c
Abhay Manoj <melchior88729@gmail.com>
2026-08-19 14:11:23 -0400

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        while (stones.size() > 1) {
+            sort(stones.begin(), stones.end());
+
+            auto first{stones.back()};
+            stones.pop_back();
+            auto second{stones.back()};
+            stones.pop_back();
+
+            auto result{max(first, second) - min(first, second)};
+            if (result != 0) {
+                stones.push_back(result);
+            }
+        }
+
+        return stones.size() == 0 ? 0 : stones[0];
+    }
+};
