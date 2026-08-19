d45121739f5714011bb37a06fa94fa41ce3eae8c
Abhay Manoj <melchior88729@gmail.com>
2026-08-19 01:32:46 -0400

    Add: sort-array-by-parity - submission-3

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        auto L{0uz}, R{nums.size() - 1};
+
+        while (L < R) {
+            auto lrem{nums[L] % 2};
+            auto rrem{nums[R] % 2};
+
+            if (lrem == 0) L++;
+            else if (rrem == 1) R--;
+            else {
+                swap(nums[L], nums[R]);
+                L++;
+            }
+        }
+
+        return nums;
+    }
+};
