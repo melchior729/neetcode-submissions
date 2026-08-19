60d4a854c7ae27e5e836f6138d4600ece696b59f
Abhay Manoj <melchior88729@gmail.com>
2026-08-18 20:16:47 -0400

    Add: sign-of-the-product-of-an-array - submission-3

---
+++ Data Structures & Algorithms/sign-of-the-product-of-an-array/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        auto sign{1}; 
+        for (const auto &n : nums) {
+            if (n == 0) {
+                return 0;
+            }
+
+            sign *= (n/abs(n));
+        }
+
+        return sign;
+    }
+};
