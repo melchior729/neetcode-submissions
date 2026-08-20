dd4cfe1a573b9020f2747fd66a1c75fa1c46f49c
Abhay Manoj <melchior88729@gmail.com>
2026-08-20 19:07:01 -0400

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        auto res{""s};
+        auto min_len(min(word1.size(), word2.size()));
+
+        auto i{0uz};
+        for (; i < min_len; ++i) {
+            res += word1[i];
+            res += word2[i];
+        }
+
+        res.append(word1.substr(i));
+        res.append(word2.substr(i));
+
+        return res;
+    }
+};
