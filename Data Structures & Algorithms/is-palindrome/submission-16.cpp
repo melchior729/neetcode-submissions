a69198b6efa4a1de3e135f76e38a084d03181ecc
Abhay Manoj <melchior88729@gmail.com>
2026-08-19 23:06:50 -0400

    Add: is-palindrome - submission-16

---
+++ Data Structures & Algorithms/is-palindrome/submission-16.cpp
@@ -0,0 +1,32 @@
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        auto n{s.size()};
+
+        auto L{0};
+        auto R{n - 1};
+
+        while (L < R) {
+            while (L < R && !isalnum(s[L])) {
+                L++;
+            }
+
+            while (L < R && !isalnum(s[R])) {
+                R--;
+            }
+
+            if (L == R) {
+                return true;
+            }
+
+            if (tolower(s[L]) != tolower(s[R])) {
+                return false;
+            }
+
+            L++;
+            R--;
+        }
+
+        return true;
+    }
+};
