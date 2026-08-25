02ea8cc127c8504bf10fafc2ce00775d866f44b4
Abhay Manoj <melchior88729@gmail.com>
2026-08-25 15:45:23 -0400

    Add: maximum-nesting-depth-of-the-parentheses - submission-0

---
+++ Data Structures & Algorithms/maximum-nesting-depth-of-the-parentheses/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        auto max_depth{0};
+        auto depth{0};
+        for (const auto &c : s) {
+            if (c == '(') {
+                depth++;
+                max_depth = max(max_depth, depth);
+            } else if (c == ')') {
+                depth--;
+            }
+        }
+
+        return max_depth;
+    }
+};
