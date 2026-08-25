5fa0f110bbe2be1803e77bf898262ee39080b93b
Abhay Manoj <melchior88729@gmail.com>
2026-08-25 06:42:38 -0400

    Add: length-of-last-word - submission-1

---
+++ Data Structures & Algorithms/length-of-last-word/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        auto end{static_cast<int>(s.size() - 1)};
+        while (end >= 0 && !isalpha(s[end])) {
+            end--;
+        }
+
+        if (end == -1) {
+            return 0;
+        }
+
+        auto space{end};
+        while (space >= 0 && s[space] != ' ') {
+            space--;
+        }
+        
+        if (space == -1) {
+            return end + 1;
+        }
+
+        return end - space;
+    }
+};
