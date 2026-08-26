852bb2f2e5fb624b5796ee3541fc709615a259a3
Abhay Manoj <melchior88729@gmail.com>
2026-08-26 18:44:55 -0400

    Add: time-needed-to-buy-tickets - submission-0

---
+++ Data Structures & Algorithms/time-needed-to-buy-tickets/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        /*
+
+        [2, 3, 2] = 7 (k = 1)
+
+
+
+
+        [2, 3, 4] = 7 (k = 1)
+
+        3 + 3 + 2 = 8
+
+        */
+
+        auto sum{0};
+        for (auto i{0uz}; i < tickets.size(); ++i) {
+            sum += min(tickets[k], tickets[i]);
+        }
+
+        return sum;
+    }
+};
