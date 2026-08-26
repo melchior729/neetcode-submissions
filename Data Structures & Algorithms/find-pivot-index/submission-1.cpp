ce1f56d5c215d41bca518bd13838a5be9936e661
Abhay Manoj <melchior88729@gmail.com>
2026-08-26 18:31:09 -0400

    Add: find-pivot-index - submission-1

---
+++ Data Structures & Algorithms/find-pivot-index/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        auto n{nums.size()};
+        vector<int> prefix(n);
+
+        prefix[0] = nums[0];
+        for (auto i{1uz}; i < n; ++i) {
+            prefix[i] = nums[i] + prefix[i - 1];
+        }
+
+        vector<int> suffix(n);
+
+        suffix[n - 1] = nums[n - 1];
+        for (auto i{n - 1}; i --> 0;) {
+            suffix[i] = nums[i] + suffix[i + 1];
+        }
+
+        for (auto i{0uz}; i < n; ++i) {
+            auto left{i > 0 ? prefix[i - 1] : 0};
+            auto right{i < n - 1 ? suffix[i + 1] : 0};
+
+            if (left == right) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
